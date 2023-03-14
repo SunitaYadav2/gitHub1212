ae34e0e00fe1589475d76ccfda8f79d2eb1fd0c1
SunitaYadav2 <yadavsunita949@gmail.com>
2023-03-14 01:30:13 -0500

    my second commit

--- src/main/java/com/MainClass.java
+++ src/main/java/com/MainClass.java
@@ -16,14 +16,14 @@ public class MainClass {
             session.save(st);
             
             
-            Student st1= new Student(2,"sunita",555);
+            Student st1= new Student(2,"binita",545);
             session.save(st1);
             
             
-            Student st2= new Student(3,"sunita",555);
+            Student st2= new Student(3,"rita",565);
             session.save(st2);
             
-            Student st3= new Student(4,"sunita",555);
+            Student st3= new Student(4,"peter",575);
             session.save(st3);
             
             
